484167e9b4b2260abb7ada33ed12f1ebe4508f95
MoinulIslam7 <moinul180180@gmail.com>
2022-11-26 17:24:37 +0600

    load data from mongodb

--- src/Pages/CategoryDetails/CategoryDetails.js
+++ src/Pages/CategoryDetails/CategoryDetails.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const CategoryDetails = () => {
-    const { id, category, products } = useLoaderData();
-    console.log(id, category, products)
+    const { _id,  products } = useLoaderData();
+    console.log(_id, products)
     return (
         <div>
             <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-1">
                 {
                     products.map(product =>
-                        <div key={product.id}>
+                        <div key={product._id}>
                             <div className="card w-96 h-5/6 bg-sky-700 mx-auto shadow-xl">
                                 <figure className="px-20 pt-20">
                                     <img  src={product.image} alt="Shoes"  />
--- src/Pages/Home/Categories/CategoriesCard.js
+++ src/Pages/Home/Categories/CategoriesCard.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CategoriesCard = ({categoryData}) => {
-    const { id, category, img } =  categoryData;
-    console.log(id)
+    const { _id, category, img } =  categoryData;
     return (
         <div className="card card-compact w-96 h-3/4 shadow-xl p-3">
             <div className="card-body">
                 <img className='w-full' src={img} alt="" />
                 <h2 className="card-title justify-center">{category}</h2>
                 <div className="card-actions justify-center">
-                    <Link to={`/categoryDetails/${id}`}>
+                    <Link to={`/categoryDetails/${_id}`}>
                         <button className=' btn btn-primary border-2 rounded px-4 py-1 '>View Product On this Category</button>
                     </Link>
                 </div>
