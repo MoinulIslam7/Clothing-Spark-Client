263b9f39501cd95f64de6b79d130161018620bc5
MoinulIslam7 <moinul180180@gmail.com>
2022-12-21 20:19:11 +0600

    contact page added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Advertise from './Advertise/Advertise';
 import Banner from './Banner/Banner';
 import Categories from './Categories/Categories';
+import Contact from './Contact/Contact';
 import SpecialOffer from './SpecialOffer/SpecialOffer';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Advertise></Advertise>
             <Categories></Categories>
             <SpecialOffer></SpecialOffer>
+            <Contact></Contact>
         </div>
     );
 };
